5ade99f2e554d36dd45d0dcdd2c7eb39b4ed82ee
Guryss <lala1470@konkuk.ac.kr>
2025-07-09 14:23:46 +0900

    project setting

---
+++ Mine4Cut/Mine4CutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mine4CutApp.swift
+//  Mine4Cut
+//
+//  Created by Seoyeon Choi on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Mine4CutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
