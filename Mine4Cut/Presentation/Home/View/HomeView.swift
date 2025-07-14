8e63e1f2da6baffff1e4ccf75938d4cdad719e01
ParkSeongGeun <phd0801@naver.com>
2025-07-14 16:23:52 +0900

    [Feat] HomeView 업데이트

--- Mine4Cut/Presentation/Home/View/HomeView.swift
+++ Mine4Cut/Presentation/Home/View/HomeView.swift
@@ -10,49 +10,18 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         ScrollView {
-            // 추천 프레임 화면
-            VStack(alignment: .leading) {
+            VStack(
+                alignment: .leading,
+                spacing: 30
+            ) {
+                // 추천 프레임 화면
                 TodayFramesView()
-            }
-            .frame(maxWidth: .infinity)
-            
-            // 이번주 인기 프레임
-            VStack(alignment: .leading) {
-                HStack {
-                    Text("이번주 인기 프레임")
-                    
-                    Spacer()
-                }
                 
+                // 이번주 인기 프레임
                 WeeklyRankingFrameView()
-            }
-            .frame(maxWidth: .infinity)
-            
-            
-            // 지금 가장 많이 사용한 프레임은?
-            VStack(alignment: .leading) {
-                HStack {
-                    Text("지금 가장 많이 사용한 프레임은?")
-                    
-                    Spacer()
-                }
                 
+                // 지금 가장 많이 사용한 프레임은?
                 TrendingFrameView()
-            }
-            .frame(maxWidth: .infinity)
-            
-            // 이 프레임은 어때요?
-            VStack(alignment: .leading) {
-                HStack {
-                    Text("이 프레임은 어때요?")
-                    Spacer()
-                }
-                HStack {
-                    Text("선호장르를 기반으로 추천해드려요")
-                    Spacer()
-                }
-                
-                PersonalFrameView()
             }
             .frame(maxWidth: .infinity)
         }
