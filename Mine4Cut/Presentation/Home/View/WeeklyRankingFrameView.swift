b92192b89b53b94b4814addf95c8ecc685e5c3d0
ParkSeongGeun <phd0801@naver.com>
2025-07-14 16:23:39 +0900

    [Feat] 이번주 인기 프레임뷰

--- Mine4Cut/Presentation/Home/View/WeeklyRankingFrameView.swift
+++ Mine4Cut/Presentation/Home/View/WeeklyRankingFrameView.swift
@@ -8,11 +8,71 @@
 import SwiftUI
 
 struct WeeklyRankingFrameView: View {
+    let frameInfos: [FrameInfo]
+    
+    // MARK: - mockFrame
+    init(frameInfos: [FrameInfo] = FrameInfo.mockFrames) {
+        self.frameInfos = frameInfos
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            VStack(
+                alignment: .leading,
+                spacing: 12
+            ) {
+                HStack {
+                    Text("이번주 인기 프레임 TOP 3")
+                        .fontWeight(.bold)
+                    Spacer()
+                }
+                
+                ScrollView(
+                    .horizontal,
+                    showsIndicators: false
+                ) {
+                    HStack(spacing: 12) {
+                        ForEach(frameInfos.indices, id: \.self) { idx in
+                            FrameImageView(
+                                frame: frameInfos[idx],
+                                size: .medium,
+                                screenWidth: geometry.size.width
+                            )
+                            .overlay (
+                                Group {
+                                    if idx >= 0 && idx <= 2 {
+                                        medalOverlay(for: idx)
+                                    }
+                                },
+                                alignment: .bottomTrailing
+                            )
+                        }
+                    }
+                }
+            }
+        }
+        // TODO: Constant로 수정
+        .frame(height: 180)
+    }
+    
+    // TODO: - 서버에서 rank 정보 넘겨줄 때 확정
+    @ViewBuilder
+    private func medalOverlay(for rank: Int) -> some View {
+        Text(medalEmoji(for: rank))
+            .font(.system(size: 20))
+    }
+    
+    private func medalEmoji(for rank: Int) -> String {
+        switch rank {
+        case 0: return "🥇"
+        case 1: return "🥈"
+        case 2: return "🥉"
+        default: return ""
+        }
     }
 }
 
 #Preview {
     WeeklyRankingFrameView()
+        .padding()
 }
