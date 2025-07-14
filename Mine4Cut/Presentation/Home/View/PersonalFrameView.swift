1bb2f013698411179e1eb2d96490811fc3debf70
ParkSeongGeun <phd0801@naver.com>
2025-07-14 18:42:41 +0900

    [Feat] '이 프레임 어때요?' 뷰

--- Mine4Cut/Presentation/Home/View/PersonalFrameView.swift
+++ Mine4Cut/Presentation/Home/View/PersonalFrameView.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct PersonalFrameView: View {
+    let frameInfos: [FrameInfo] = FrameInfo.mockFrames
+    let parentSize: CGSize
+    
+    init(parentSize: CGSize) {
+        self.parentSize = parentSize
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        let columns = [
+            GridItem(.flexible(), spacing: 16),
+            GridItem(.flexible())
+        ]
+        
+        let gridSpacing: CGFloat = 16
+        
+        ScrollView(showsIndicators: false) {
+            VStack(
+                alignment: .leading,
+                spacing: 4
+            ) {
+                Text("이 프레임은 어때요?")
+                    .font(.system(size: 16, weight: .bold))
+                
+                Text("선호장르를 기반으로 추천해드려요")
+                    .font(.system(size: 14))
+                    .foregroundColor(.gray)
+                    .padding(.bottom, 8)
+                
+                LazyVGrid(columns: columns, spacing: gridSpacing) {
+                    ForEach(frameInfos.indices, id: \.self) { idx in
+                        FrameImageView(
+                            frame: frameInfos[idx],
+                            size: .large,
+                            screenWidth: parentSize.width
+                        )
+                    }
+                }
+            }
+            .padding(.horizontal)
+        }
     }
 }
 
 #Preview {
-    PersonalFrameView()
+    GeometryReader { geometry in
+        PersonalFrameView(parentSize: geometry.size)
+    }
 }
