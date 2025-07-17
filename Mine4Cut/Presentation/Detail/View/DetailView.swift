e9dcb0c1ae604a9c4d1f14c01c32df0e187d4d2a
ParkSeongGeun <phd0801@naver.com>
2025-07-17 11:37:50 +0900

    [Chore] Make Dummy DetailView Files

---
+++ Mine4Cut/Presentation/Detail/View/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Mine4Cut
+//
+//  Created by 박성근 on 7/17/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
